8b053ddde61750cb0ce6d9fad720e5deac1085a7
davidbeleznay <dbeleznay09@gmail.com>
2025-04-30 14:52:35 -0700

    Simplify metro config for Expo SDK 47

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,9 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Increase timeout for transforming files
-config.transformer.transformerPath = require.resolve('metro-transform-worker');
-config.transformer.workerPath = require.resolve('metro/src/DeltaBundler/Worker');
-config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json'];
-config.server.port = 8082;
+// Increase timeout for slow connections
 config.server.timeout = 120000; // 2 minutes timeout
 config.maxWorkers = 2; // Reduce number of workers to avoid timeouts
 
